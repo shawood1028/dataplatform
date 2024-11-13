edb1f7f4acecfdb2d348f76a62d766028b7b320d
shawood1028 <shawood1028@gmail.com>
2024-11-13 17:31:09 +0800

    feat: add doubao AI service demo

--- dp-be/src/main/java/com/shawood/service/AIServiceFactory.java
+++ dp-be/src/main/java/com/shawood/service/AIServiceFactory.java
@@ -1,5 +1,6 @@
 package com.shawood.service;
 
+import com.shawood.service.impl.DoubaoServiceImpl;
 import com.shawood.service.impl.OpenAIServiceImpl;
 import com.shawood.service.intf.AIService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +9,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class AIServiceFactory {
     private final OpenAIServiceImpl openAIService;
+    private final DoubaoServiceImpl doubaoService;
 
     @Autowired
-    public AIServiceFactory(OpenAIServiceImpl openAIService) {
+    public AIServiceFactory(OpenAIServiceImpl openAIService, DoubaoServiceImpl doubaoService) {
         this.openAIService = openAIService;
+        this.doubaoService = doubaoService;
     }
 
     public AIService createService(String platform) throws IllegalAccessException {
         switch (platform.toLowerCase()) {
             case "openai":
                 return openAIService;
+            case "doubao":
+                return doubaoService;
             default:
                 throw new IllegalAccessException("Unsupported platform: " + platform);
         }
--- dp-be/src/main/java/com/shawood/service/impl/DoubaoServiceImpl.java
+++ dp-be/src/main/java/com/shawood/service/impl/DoubaoServiceImpl.java
@@ -2,12 +2,23 @@ package com.shawood.service.impl;
 
 import com.shawood.service.intf.AIService;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+@Service
 public class DoubaoServiceImpl implements AIService {
 
     @Value("${aiservers.doubaoapikey}")
+    private String apikey;
+
+    private final RestTemplate restTemplate;
+
+    public DoubaoServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @Override
     public String generateText(String prompt) {
-        return "";
+        return "doubao resp 321";
     }
 }
